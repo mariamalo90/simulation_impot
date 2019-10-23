acaf3cf5fb79ccf9f27f447bf117883338009887
Mariama LOi <mariamalo90@gmail.com>
2019-10-23 00:51:23 +0000

    creation du projet spring boot

---
+++ src/main/java/com/simulation/impots/simulation_impot/SimulationImpotApplication.java
@@ -0,0 +1,13 @@
+package com.simulation.impots.simulation_impot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SimulationImpotApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SimulationImpotApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/simulation/impots/simulation_impot/SimulationImpotApplicationTests.java
@@ -0,0 +1,13 @@
+package com.simulation.impots.simulation_impot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SimulationImpotApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
